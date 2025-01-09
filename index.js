ec29ce4038afa585cb79600967300c0749707887
MatthewAgapay <matthew.agapay@hotmail.com>
2025-01-09 10:53:44 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 window.onload = function() {
   function displayTime() {
     var currentTime = new Date();
-   
     var hours = currentTime.getHours();
-   
     var minutes = currentTime.getMinutes();
-   
     var seconds = currentTime.getSeconds();
-   
     var clockDiv = document.getElementById('clock');
-    
-    var meridiem = "AM";
-   
+   var meridiem = "AM";
    if (seconds < 10) {
-   
     seconds = "0" + seconds;
 }
-
 if (hours > 12) {
     hours = hours - 12;
     meridiem = "PM";
